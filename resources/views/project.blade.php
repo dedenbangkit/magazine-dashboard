f0355896d909f8b3223cda080fd91ea7fbb8a4fd
dedenbangkit <mail@dedenbangkit.com>
2017-06-15 22:20:59 +0700

    layout published content

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -5,12 +5,10 @@
 @endsection
 
 @section('main-content')
-        <div class="row">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading"><?=  ucfirst(array_last($activer));?></div>
-                        <div class="panel-body">
+                    <div class="box-header with-border">
+                        <div class="box-body">
                             <!-- <?php if($create == true) {?>
                             <div class="col-md-3  text-center">
                                 <div class="box-content" style="cursor: pointer;" onclick="window.location='/create-project';">
@@ -22,30 +20,30 @@
                             <?php } ?> -->
                             <?php foreach($projects as $i=>$row){ ?>
                             <div class="col-sm-3  text-center">
-                                <div class="box-content" style="cursor: pointer;" onclick="window.location='/issue?id={{$row->id}}';">
-                                    <div class="box-content-inside">
-                                        <div class="row">
-                                            <img src="{{asset('/img/projects/'.$row->issue_cover)}}">
-                                        </div>
-                                        <div class="row">
-                                            <b>{{$row->issue_name}}</b>
-                                        </div>
-                                        <div class="row">
-                                            <p class="font-light"
-                                               style="color: green">{{date('d-m-Y',strtotime($row->updated_at))}}</p>
-                                        </div>
-                                    </div>
+                                <div class="box box-solid">
+                                  <div class="box-header with-border" style="border-top: 3px solid #d2d6de;">
+                                    <i class="fa fa-file-text-o"></i>
+
+                                    <h3 class="box-title"><?php echo(empty($row->issue_name) ? 'Untittled' : $row->issue_name); ?></h3>
+                                    <br>Published at: {{date('d-m-Y',strtotime($row->updated_at))}}
+                                  </div>
+
+                                  <div class="box-body">
+                                    <img class="img-responsive" src="<?php if (empty($row->issue_cover)) {
+                                        echo asset('/img/empty_zine.jpg');
+                                    } else {
+                                        echo $row->issue_cover;
+                                    } ?>">
+                                  </div>
+
                                 </div>
                             </div>
                             <?php } ?>
 
 
                         </div>
-                        <div class="pull-right" style="margin-right:20px;">
-                            {{ $projects->links() }}
-                        </div>
                     </div>
                 </div>
-            </div>
+              </div>
 
 @endsection
