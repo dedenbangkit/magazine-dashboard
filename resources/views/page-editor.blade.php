75a8ea8c1276177911a872c1cbb236e6851fe49c
dedenbangkit <mail@dedenbangkit.com>
2017-06-16 10:35:30 +0700

    async

--- resources/views/page-editor.blade.php
+++ resources/views/page-editor.blade.php
@@ -2150,7 +2150,7 @@
         }).done(function(data) {
             $(data['page_list']).each(function(index, el) {
                 page_content = el.page_content;
-                newPage(el.page_name,el.page_content,el.id,index)
+                newPage(el.page_name,el.page_content,el.id,index);
                 contentInject();
             });
         });
