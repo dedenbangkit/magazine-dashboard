47cdd8b203d1ce99c125f48f2b66d91dd7e6a424
dedenbangkit <mail@dedenbangkit.com>
2017-07-03 17:50:58 +0700

    replace all symbol to regular expression

--- storage/application/www/js/controllers.js
+++ storage/application/www/js/controllers.js
@@ -123,7 +123,7 @@ angular.module('starter.controllers', ['ionic', 'ui.router', 'ngSanitize'])
               thing.progress = StorageService.cacheIssue(thing.folderName);
               thing.magazineId = thing.magazineId;
               thing.issueName = thing.issueName;
-              thing.issueRegex = thing.issueName.replace(/#/g, "");
+              thing.issueRegex = thing.issueName.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
               $http.get('http://api-dev.publixx.id/issue/' + thing.magazineId + '/MagzApis/')
                 .success(function(data) {
                   $localStorage.content['issue-' + thing.magazineId] = data.results;
