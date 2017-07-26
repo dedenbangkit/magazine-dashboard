c1bf3bf14276f32895480db824e11ae6fd1de65c
Rizky Rahardian Akbar <rahardian.rizky@gmail.com>
2017-07-26 17:06:17 +0700

    mail controller

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use Mail;
 class EmailController extends Controller
 {
     public function send(Request $request)
